d294d9183b5e9a385ff1a93e7592f001c01e9912
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-07-28 20:58:10 +0700

    Hoan thanh bai thuc hanh

---
+++ src/app/likes/likes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-likes',
+  templateUrl: './likes.component.html',
+  styleUrls: ['./likes.component.css']
+})
+export class LikesComponent implements OnInit {
+  like: number =0;
+  addLike(){
+    this.like++;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
